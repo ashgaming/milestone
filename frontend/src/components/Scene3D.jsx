535050ab7be5c883847766bf72fbd60d0c41b303
unknown <ashalhat10000@gmail.com>
2025-04-04 17:07:49 +0530

    hero

--- frontend/src/components/Scene3D.jsx
+++ frontend/src/components/Scene3D.jsx
@@ -2,7 +2,7 @@
 // import { OrbitControls, PerspectiveCamera, Environment } from '@react-three/drei';
 // import { Building3D } from './Building3D';
 
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 
 // Custom loading component (you can style this however you like)
 const LoadingIndicator = () => (
@@ -11,21 +11,14 @@ const LoadingIndicator = () => (
 );
 
 const Scene3D = memo(() => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  const handleLoad = () => {
-    setIsLoaded(true);
-  };
 
   return (
     <div className="h-screen w-full">
-      {/* {!isLoaded && <LoadingIndicator />} */}
       <iframe
         src="https://skybox.blockadelabs.com/e/52568c1efc576509ae58f5f3735aba1e"
-        className={`w-full h-full ${isLoaded ? "block" : "hidden"}`}
+        className={`w-full h-full `}
         allow="fullscreen"
         loading="lazy"
-        onLoad={handleLoad} // Triggered when iframe content finishes loading
       />
     </div>
   );
